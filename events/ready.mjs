d49889a6ed7fd7e06628f02511ac12d9425a07fb
Smultar <Jason@smultar.com>
2022-01-30 23:54:50 -0500

    Mozu Remake

--- events/ready.mjs
+++ events/ready.mjs
@@ -10,6 +10,8 @@ export default async () => {
         // Client Console Update
         client.user.setActivity('Bizu Scream', { type: 'LISTENING', status: 'online'});
         
+        // Mozu Ready
+
         // Client Presence Update
         console.log(`Connection System: ${client.user.tag} is connected to Discord's servers.`);
     })
