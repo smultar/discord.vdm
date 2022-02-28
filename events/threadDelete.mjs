ac28bd9885c521e4f4b129813e07de4c7c25b117
Smultar <Jason@smultar.com>
2022-02-28 03:04:36 -0500

    Added function that closes active conversations if the thread was deleted

--- events/threadDelete.mjs
+++ events/threadDelete.mjs
@@ -1,11 +1,28 @@
 import { WebhookClient } from 'discord.js';
 import client from '../index.mjs';
-import { write, read, fetchAll } from '../database/index.js';
+import { write, read, fetchAll, remove } from '../database/index.js';
 
 export default async () => {
 
     client.on('threadDelete', async (thread) => {
         console.log('Deleted Thread:', thread);
+
+        let target = client.messages.find(u => u.thread === thread.id);
+
+        if (target) {
+
+            try {
+                await client.users.cache.get(target.id).send('Your conversation has been closed.');
+            } catch (error) {
+                console.log('Could not send "close" message to user.');
+            }
+
+            client.messages.delete(target.id);
+            await remove("mes", target.id); console.log(remove);
+
+        } else {
+            console.log('Could not find thread');
+        }
 
     });
  
